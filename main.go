f546951af862f7c5942aba7b1706df427fda25d6
AMalkov07 <andrey.malkov07@gmail.com>
2021-12-23 23:03:50 -0600

    added atom and variables blocks and query and predicate stmts

--- main.go
+++ main.go
@@ -1,6 +1,21 @@
 package main
 
+import "fmt"
+
 func main() {
-	testtest()
-	testtest()
+	var iTest []stmt
+	aTest := atom{str: "this is atom str"}
+	vTest := variable{str: "this is variable str"}
+	cTest2 := compound{head: "head2",
+		body: vTest}
+	cTest := compound{head: "head1",
+		body: cTest2}
+	qTest := quer{body: aTest}
+	pTest := pred{body: cTest}
+	iTest = append(iTest, qTest)
+	iTest = append(iTest, pTest)
+	for _, e := range iTest {
+		e.stmtShow()
+		fmt.Println()
+	}
 }
